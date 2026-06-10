813d04c16c89cbf1499f6b3ede290b870062ca52
김혜빈 센서개발팀 <dev.5igner1101@gmail.com>
2026-06-10 15:59:44 +0900

    root path

--- InstanceSegmentationEval/InstanceSegmentationEval.py
+++ InstanceSegmentationEval/InstanceSegmentationEval.py
@@ -2,56 +2,73 @@ import json
 import torch
 from pathlib import Path
 
+import numpy as np
+from PIL import Image
+from rfdetr import RFDETRSegMedium
+from pycocotools import mask as mask_utils
+
+PROJECT_ROOT = Path.cwd()
+
+MODEL_PATH = PROJECT_ROOT / "IS_pretrained_bottom.pt"
+DATASET_ROOT = PROJECT_ROOT / "dataset-coco-seg" / "dataset-coco-seg"
+
+VALID_DIR = DATASET_ROOT / "valid"
+TEST_DIR = DATASET_ROOT / "test"
+
+VALID_ANN_PATH = VALID_DIR / "_annotations.coco.json"
+TEST_ANN_PATH = TEST_DIR / "_annotations.coco.json"
+
+print("\n")
+print("-------------------------- PATH --------------------------")
+
+print("PROJECT_ROOT:", PROJECT_ROOT)
+print("MODEL_PATH:", MODEL_PATH)
+print("MODEL exists:", MODEL_PATH.exists())
+print("VALID_ANN exists:", VALID_ANN_PATH.exists())
+print("TEST_ANN exists:", TEST_ANN_PATH.exists())
+
+print("\n")
 print("-------------------------- DATA SET --------------------------")
+with open(VALID_ANN_PATH, "r", encoding="utf-8") as f:
+    coco_data = json.load(f)
 
-dataset_root = Path("dataset-coco-seg/dataset-coco-seg")
+images = coco_data["images"]
+print("valid image count:", len(images))
 
-valid_ann_path = dataset_root / "valid" / "_annotations.coco.json"
-test_ann_path = dataset_root / "test" / "_annotations.coco.json"
+#with open(VALID_ANN_PATH, "r", encoding="utf-8") as f:
+#    valid_data = json.load(f)
 
-print("valid annotation:", valid_ann_path.exists())
-print("test annotation:", test_ann_path.exists())
-print("model:", Path("IS_pretrained_bottom.pt").exists())
+#with open(TEST_ANN_PATH, "r", encoding="utf-8") as f:
+#    test_data = json.load(f)
 
-with open(valid_ann_path, "r", encoding="utf-8") as f:
-    valid_data = json.load(f)
+#print("valid images:", len(valid_data["images"]))
+#print("valid annotations:", len(valid_data["annotations"]))
+#print("test images:", len(test_data["images"]))
+#print("test annotations:", len(test_data["annotations"]))
 
-with open(test_ann_path, "r", encoding="utf-8") as f:
-    test_data = json.load(f)
-
-print("valid images:", len(valid_data["images"]))
-print("valid annotations:", len(valid_data["annotations"]))
-print("test images:", len(test_data["images"]))
-print("test annotations:", len(test_data["annotations"]))
-
-
-
-print("valid annotations:", len(valid_data["annotations"]))
-
-
-print("\n\n")
+print("\n")
 print("-------------------------- PRETRAINED MODEL --------------------------")
 
-model_path = Path("IS_pretrained_bottom.pt")
+model = RFDETRSegMedium.from_checkpoint(str(MODEL_PATH))
+#ckpt = torch.load(
+#    MODEL_PATH,
+#    map_location="cpu",
+#    weights_only=False
+#)
 
-print("model exists:", model_path.exists())
+#print("model load success")
+#print("checkpoint type:", type(ckpt))
 
-ckpt = torch.load(
-    model_path,
-    map_location="cpu",
-    weights_only=False
-)
+#if isinstance(ckpt, dict):
+#    print("checkpoint keys:", ckpt.keys())
 
-print("model load success")
-print("checkpoint type:", type(ckpt))
+#    if "args" in ckpt:
+#        print("args:")
+#        print(ckpt["args"])
 
-if isinstance(ckpt, dict):
-    print("checkpoint keys:", ckpt.keys())
+#    if "model" in ckpt:
+#        print("model weights type:", type(ckpt["model"]))
+#        print("number of weight keys:", len(ckpt["model"]))
 
-    if "args" in ckpt:
-        print("args:")
-        print(ckpt["args"])
-
-    if "model" in ckpt:
-        print("model weights type:", type(ckpt["model"]))
-        print("number of weight keys:", len(ckpt["model"]))
+print("\n")
+print("-------------------------- INFERENCE --------------------------")
