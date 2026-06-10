1af7d0b6d17a9c63a83f831d99684a7201734cd6
김혜빈 센서개발팀 <dev.5igner1101@gmail.com>
2026-06-10 15:31:48 +0900

    model load

--- InstanceSegmentationEval/InstanceSegmentationEval.py
+++ InstanceSegmentationEval/InstanceSegmentationEval.py
@@ -1,5 +1,8 @@
 import json
+import torch
 from pathlib import Path
+
+print("-------------------------- DATA SET --------------------------")
 
 dataset_root = Path("dataset-coco-seg/dataset-coco-seg")
 
@@ -20,3 +23,35 @@ print("valid images:", len(valid_data["images"]))
 print("valid annotations:", len(valid_data["annotations"]))
 print("test images:", len(test_data["images"]))
 print("test annotations:", len(test_data["annotations"]))
+
+
+
+print("valid annotations:", len(valid_data["annotations"]))
+
+
+print("\n\n")
+print("-------------------------- PRETRAINED MODEL --------------------------")
+
+model_path = Path("IS_pretrained_bottom.pt")
+
+print("model exists:", model_path.exists())
+
+ckpt = torch.load(
+    model_path,
+    map_location="cpu",
+    weights_only=False
+)
+
+print("model load success")
+print("checkpoint type:", type(ckpt))
+
+if isinstance(ckpt, dict):
+    print("checkpoint keys:", ckpt.keys())
+
+    if "args" in ckpt:
+        print("args:")
+        print(ckpt["args"])
+
+    if "model" in ckpt:
+        print("model weights type:", type(ckpt["model"]))
+        print("number of weight keys:", len(ckpt["model"]))
