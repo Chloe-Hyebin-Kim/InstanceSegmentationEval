13f90d0d448c12529bb17090921857ae1db52581
김혜빈 센서개발팀 <dev.5igner1101@gmail.com>
2026-06-10 17:46:56 +0900

    valid prediction

--- InstanceSegmentationEval/InstanceSegmentationEval.py
+++ InstanceSegmentationEval/InstanceSegmentationEval.py
@@ -7,6 +7,9 @@ from PIL import Image
 from rfdetr import RFDETRSegMedium
 from pycocotools import mask as mask_utils
 
+from pycocotools.coco import COCO
+from pycocotools.cocoeval import COCOeval
+
 PROJECT_ROOT = Path.cwd()
 
 MODEL_PATH = PROJECT_ROOT / "IS_pretrained_bottom.pt"
@@ -18,33 +21,35 @@ TEST_DIR = DATASET_ROOT / "test"
 VALID_ANN_PATH = VALID_DIR / "_annotations.coco.json"
 TEST_ANN_PATH = TEST_DIR / "_annotations.coco.json"
 
+VALID_OUTPUT_JSON_PATH = PROJECT_ROOT / "valid_predictions.json"
+TEST_OUTPUT_JSON_PATH = PROJECT_ROOT / "test_predictions.json"
+
 print("\n")
 print("-------------------------- PATH --------------------------")
 
-print("PROJECT_ROOT:", PROJECT_ROOT)
-print("MODEL_PATH:", MODEL_PATH)
 print("MODEL exists:", MODEL_PATH.exists())
 print("VALID_ANN exists:", VALID_ANN_PATH.exists())
 print("TEST_ANN exists:", TEST_ANN_PATH.exists())
 
 print("\n")
 print("-------------------------- DATA SET --------------------------")
+
 with open(VALID_ANN_PATH, "r", encoding="utf-8") as f:
-    coco_data = json.load(f)
+    valid_data = json.load(f)
+    valid_images = valid_data["images"]
 
-images = coco_data["images"]
-print("valid image count:", len(images))
+with open(TEST_ANN_PATH, "r", encoding="utf-8") as f:
+    test_data = json.load(f)
+    test_images = test_data["images"]
 
-#with open(VALID_ANN_PATH, "r", encoding="utf-8") as f:
-#    valid_data = json.load(f)
 
-#with open(TEST_ANN_PATH, "r", encoding="utf-8") as f:
-#    test_data = json.load(f)
+print("valid images:", len(valid_data["images"]))
+print("valid image count:", len(valid_images))
+print("valid annotations:", len(valid_data["annotations"]))
 
-#print("valid images:", len(valid_data["images"]))
-#print("valid annotations:", len(valid_data["annotations"]))
-#print("test images:", len(test_data["images"]))
-#print("test annotations:", len(test_data["annotations"]))
+print("test images:", len(test_data["images"]))
+print("test image count:", len(test_images))
+print("test annotations:", len(test_data["annotations"]))
 
 print("\n")
 print("-------------------------- PRETRAINED MODEL --------------------------")
@@ -71,19 +76,18 @@ model = RFDETRSegMedium.from_checkpoint(str(MODEL_PATH))
 #        print("number of weight keys:", len(ckpt["model"]))
 
 print("\n")
-print("-------------------------- INFERENCE --------------------------")
+print("-------------------------- VALID INFERENCE --------------------------")
 
 predictions = []
 
-for idx, image_info in enumerate(images, start=1):
+for idx, image_info in enumerate(valid_images, start=1):
     image_id = image_info["id"]
     file_name = image_info["file_name"]
     image_path = VALID_DIR / file_name
 
-    print(f"[{idx}/{len(images)}] inference: {file_name}")
+    print(f"[{idx}/{len(valid_images)}] inference: {file_name}")
 
     image = Image.open(image_path).convert("RGB")
-
     detections = model.predict(image, threshold=0.05)
 
     if detections.mask is None:
@@ -99,30 +103,19 @@ for idx, image_info in enumerate(images, start=1):
         rle = mask_utils.encode(mask)
         rle["counts"] = rle["counts"].decode("utf-8")
 
-        # 중요:
-        # 모델 class_id는 0부터 background, band, beltloop...
-        # COCO category_id는 1부터 background, band, beltloop...
-        # 그래서 +1 보정 필요
-        coco_category_id = int(class_id) + 1
-
         predictions.append({
             "image_id": int(image_id),
-            "category_id": coco_category_id,
+            "category_id": int(class_id),
             "segmentation": rle,
             "score": float(score),
         })
 
-
         
 print("\n")
-print("-------------------------- PREDICTION JSON --------------------------")
+print("-------------------------- SAVE VALID RESULT --------------------------")
 
-OUTPUT_JSON_PATH = PROJECT_ROOT / "valid_predictions.json"
-
-with open(OUTPUT_JSON_PATH, "w", encoding="utf-8") as f:
+with open(VALID_OUTPUT_JSON_PATH, "w", encoding="utf-8") as f:
     json.dump(predictions, f)
 
-print()
-print("-------------------------- SAVE RESULT --------------------------")
-print("saved:", OUTPUT_JSON_PATH)
+print("saved:", VALID_OUTPUT_JSON_PATH)
 print("prediction count:", len(predictions))
